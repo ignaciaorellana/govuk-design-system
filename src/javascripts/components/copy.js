c7322d26eb8a78a49397e078f5be8defa879b663
Jani Kraner <jani.kraner@gmail.com>
2018-02-12 13:54:34 +0000

    Add aria-live to anounce action taken

--- src/javascripts/components/copy.js
+++ src/javascripts/components/copy.js
@@ -7,7 +7,7 @@
   // This module is dependent on /vendor/clipboard.js
   GOVUK.copy = {
     init: function (selector) {
-      $(selector).parent().prepend('<a class="govuk-link app-c-link--copy" href="#copy">Copy</a>')
+      $(selector).parent().prepend('<a class="govuk-link app-c-link--copy" href="#copy" aria-live="assertive">Copy</a>')
       // Copy to clipboard
       try {
         new Clipboard('.app-c-link--copy', {
